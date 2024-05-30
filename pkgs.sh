aedbc255f76acb9019c065baf4f1955aa8001a5e
cloveds <147108292+cloveds@users.noreply.github.com>
2024-05-30 11:17:19 -0500

    Update pkgs.sh

--- pkgs.sh
+++ pkgs.sh
@@ -5,3 +5,6 @@ cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
 git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 cd ~/.pyenv && src/configure && make -C src
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
