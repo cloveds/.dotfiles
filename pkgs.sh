9fd0fb43762e01476d493b6397a652a548ada48a
cloveds <147108292+cloveds@users.noreply.github.com>
2024-05-30 11:13:54 -0500

    Update pkgs.sh

--- pkgs.sh
+++ pkgs.sh
@@ -1,4 +1,5 @@
-sudo pacman -S npm hyprland waybar kitty base-devel cmake unzip ninja curl xz openssl zlib tk
+sudo pacman -S npm hyprland waybar kitty base-devel cmake unzip ninja curl 
+
 git clone https://github.com/neovim/neovim
 cd neovim && make CMAKE_BUILD_TYPE=RelWithDebInfo
 sudo make install
